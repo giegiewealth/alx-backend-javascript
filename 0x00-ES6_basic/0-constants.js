52b979223f3c3208d947f79f533504feceaa6010
root <angelamarfo17@gmail.com>
2024-04-09 13:54:44 +0000

    tests

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Changed var to const
-  return task;
+const task = 'I prefer const when I can.'; // Changed var to const
+return task;
 }
 
 export function getLast() {
-  return 'is okay';
+return 'is okay';
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Changed var to let
-  combination += getLast(); // This is allowed with let since it's a reassignment
+let combination = 'But sometimes let '; // Changed var to let
+combination += getLast(); // This is allowed with let since it's a reassignment
 
-  return combination;
+return combination;
 }
