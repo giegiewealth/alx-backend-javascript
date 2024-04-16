0dc52a0a89921f4f5ca9040e86a33b242c36a18e
root <angelamarfo17@gmail.com>
2024-04-16 23:50:53 +0000

    tweak

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,7 +6,7 @@
  *   location: String
  * }[]} students - The list of students.
  * @param {String} city - The location.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Angela Marfo
  * @returns
  */
 export default function getStudentsByLocation(students, city) {
